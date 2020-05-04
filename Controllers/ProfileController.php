5d6464217bde339028ab39130e2a64fb588f4d3f
Igor Shirockov <igor.sickterra@gmail.com>
2020-05-04 22:48:26 +0600

    finish

--- Controllers/ProfileController.php
+++ Controllers/ProfileController.php
@@ -5,18 +5,19 @@ class ProfileController
 	protected $data;
 	protected $user_id;
 	protected $user;
-	protected $pag_count = 25;
+	protected $pag_count = 20;
 
 	function __construct($data)
 	{
 		$this->data = $data;
 		$db = new DB();
+
 		$this->user_id = $db::getValue("SELECT `user_id` FROM `user_tokens` WHERE `id` = ?", [ $data['token'] ]);
+
 		if($this->user_id !== false){
 			$query = 'SELECT *, X(`last_coords`) as lat, Y(`last_coords`) as lon FROM `users` WHERE `id` = ?';
 			$this->user = $db::getRow($query, [ $this->user_id ]);
 			unset($this->user['last_coords']);
-			//die(print_r($this->user));
 		}
 	}
 
@@ -49,6 +50,7 @@ class ProfileController
 		$interests = $db::getRows('SELECT * FROM `interests`');
 		$query = 'SELECT * FROM `user_interests` WHERE `user_id` = ?';
 		$user_interests = $db::getRows($query, [ $this->user_id ]);
+
 		$response = [
 			'status' => 'success',
 			'user' => $this->user,
@@ -56,7 +58,7 @@ class ProfileController
 			'user_interests' => $user_interests,
 			'age_groups' => $age_groups
 		];
-		//die(print_r($response));
+
 		return $response;
 	}
 
@@ -96,7 +98,7 @@ class ProfileController
 		$new_arr = array_diff($interests_ids, $old_interests_ids);
 		$delete_arr = array_diff($old_interests_ids, $interests_ids);
 		$update_arr = array_diff($interests_ids, $new_arr);
-
+		// Новые записи
 		if(!empty($new_arr)) {
 			foreach ($new_arr as $id)
 			{
@@ -109,7 +111,7 @@ class ProfileController
 				]);
 			}
 		}
-
+		// На удаление
 		if (!empty($delete_arr)) {
 			foreach ($delete_arr as $id)
 			{
@@ -120,7 +122,7 @@ class ProfileController
 				]);
 			}
 		}
-
+		// На обновление
 		if (!empty($update_arr)) {
 			foreach ($update_arr as $id)
 			{
@@ -141,76 +143,65 @@ class ProfileController
 	protected function users_list()
 	{
 		$db = new DB();
+		// Возрастная группа пользователя
 		$sql = 'SELECT `id` FROM `age_groups` WHERE (`from` IS NULL AND `to` > :age) OR (`to` IS NULL AND `from` < :age) OR (`from` < :age AND `to` > :age)';
-
 		$user_age_group = $db::getValue($sql, [ 'age' => $this->user['age'] ]);
 
-		$age_from = $this->data['age_from'];
-		$age_to = $this->data['age_to'];
+		$location_type = $this->data['location_type']; // world, country, nearby
 
-		$sql = "SELECT * FROM `users` WHERE `id` <> {$this->user_id} ";
+		$select = 'u.id, u.name, u.email, u.photo, u.age, u.country, u.created_at ';
+		$from_and_join = 'FROM `users` as u
+			LEFT JOIN `user_interests` as ui ON(ui.user_id = u.id) ';
 
+		$where = "WHERE u.id <> {$this->user_id} ";
+		// не показывать профиль возрастной группе
+		$where .= "AND (`dont_show_age_group` IS NULL OR `dont_show_age_group` NOT LIKE '%{$user_age_group}%') ";
 		// фильтр по возрасту
-		if (!empty($age_from) && !empty($age_to)) {
-			$sql .= "AND `age` BETWEEN $age_from AND $age_to ";
-		}
-		else if (!empty($age_from)) {
-			$sql .= "AND `age` > $age_from ";
-		}
-		else if (!empty($age_to)) {
-			$sql .= "AND `age` < $age_to ";
-		}
+		$this->set_ages($where);
+		// Фильтрация по интересам
+		$this->set_interests($where);
 
-		$sql .= "AND `dont_show_age_group` NOT LIKE '%{$user_age_group}%' ";
+		$group = 'GROUP BY u.id ORDER BY u.created_at DESC ';
 
-		//фильтр по интересам
-		// [{"id":1,"value":1},{"id":2,"value":0},{"id":3,"value":1}]
-		$interests_on = json_decode($this->$data['interests_on']);
-
-		if (!empty($interests_on))
+		if($location_type == 'world')
 		{
-			$inter_sql_on = 'SELECT `user_id` FROM `user_interests` WHERE 1 ';
-			foreach ($interests_on as $row) {
-				$inter_sql_on .= "AND (`id` = {$row['id']} AND `value` = {$row['value']}) ";
-			}
-			$interests_user_ids = $db::getColumn($inter_sql_on);
-			$interests_user_ids_str = implode(',', $interests_user_ids);
-
-			$sql .= "AND `id` IN ($interests_user_ids_str) ";
+			$sql = 'SELECT ' . $select . $from_and_join . $where . $group;
 		}
-
-		// [{"id":1,"value":1},{"id":2,"value":0},{"id":3,"value":1}]
-		$interests_off = json_decode($this->$data['interests_off']);
-		if (!empty($interests_off))
+		else if($location_type == 'country')
 		{
-			$inter_sql_off = 'SELECT `user_id` FROM `user_interests` WHERE 1 ';
-			foreach ($interests_off as $row) {
-				$inter_sql_off .= "AND (`id` = {$row['id']} AND `value` = {$row['value']}) ";
-			}
-			$interests_user_ids = $db::getColumn($inter_sql_off);
-			$interests_user_ids_str = implode(',', $interests_user_ids);
-
-			$sql .= "AND `id` NOT IN ($interests_user_ids_str) ";
+			$sql ='SELECT * FROM ' . '(' .
+			'(SELECT 1 as rank, ' . $select . $from_and_join . $where . "AND u.country = '{$this->user['country']}' $group) UNION " .
+			'(SELECT 2 as rank, ' . $select . $from_and_join . $where . $group . ')' .
+			') AS a ' . 'GROUP BY id ORDER BY rank ASC';
 		}
+		else if($location_type == 'nearby')
+		{
+			$lat = $this->user['lat'];
+			$lon = $this->user['lon'];
+			$max_distance = 50;
+			// Выборка дистанции
+			$sql_select_coords = ", ((ACOS(
+				SIN($lat * PI() / 180) * SIN(X(`last_coords`) * PI() / 180) +
+				COS($lat * PI() / 180) * COS(X(`last_coords`) * PI() / 180) *
+				COS(($lon - Y(`last_coords`)) * PI() / 180)) * 180 / PI()
+			) * 60 * 1.1515 * 1.61) AS `distance` ";
 
-		//Расстояние
-		$lat = $this->user['lat'];
-		$long = $this->user['lon'];
-		$kmInDeg  = $this->LatLngDist([$lat, $long], [$lat, ((int)$long + 1)]);
+			$group_dop = "GROUP BY u.id HAVING `distance` <= $max_distance ORDER BY `distance` ASC, u.created_at DESC ";
 
-		$degInKm = 1 / $kmInDeg;
-
-		$r = 50; // radius = 50km
-		$rdeg = $r * $degInKm;
-		$sq[0] = array($lat - $rdeg, $long + $rdeg); // левый верхний
-		$sq[1] = array($lat + $rdeg, $long + $rdeg); // правый верхний
-		$sq[2] = array($lat + $rdeg, $long - $rdeg); // правый нижний
-		$sq[3] = array($lat - $rdeg, $long - $rdeg); // левый нижний
-		$sq[4] = $sq[0]; // замыкаем полигон
-		$polygon = "{$sq[0][0]} {$sq[0][1]}, {$sq[1][0]} {$sq[1][1]}, {$sq[2][0]} {$sq[2][1]}, {$sq[3][0]} {$sq[3][1]}, {$sq[4][0]} {$sq[4][1]}";
-		//die(print_r($polygon));
-		$sql .= "AND MBRWithin(`last_coords`, GeomFromText('Polygon(($polygon))'))";
-		$sql .= "HAVING X(`last_coords`) * X(`last_coords`) + Y(`last_coords`) * Y(`last_coords`) <= $rdeg * $rdeg";
+			$sql ='SELECT * FROM ' . '(' .
+			'(SELECT 1 as rank, ' . $select . $sql_select_coords . $from_and_join . $where . "$group_dop) UNION " .
+			'(SELECT 2 as rank, ' . $select . ', NULL AS `distance` '  . $from_and_join . $where . "AND u.country = '{$this->user['country']}' $group) UNION " .
+			'(SELECT 3 as rank, ' . $select . ', NULL AS `distance` ' . $from_and_join . $where . $group . ')' .
+			') AS a ' . 'GROUP BY id ORDER BY rank ASC';
+		}
+		// Пагинация, использующаяся при прокрутке
+		if(!empty($this->data['pagination']))
+		{
+			$pag = $this->data['pagination'] - 1;
+			$pag_from = $pag * $this->pag_count;
+			$pag_to = $pag_from + $this->pag_count;
+			$sql .= " LIMIT $pag_from, $pag_to";
+		}
 
 		$users_list = $db::getRows($sql);
 
@@ -221,16 +212,106 @@ class ProfileController
 		];
 	}
 
-	private function LatLngDist($p, $q) {
-        $R = 6371; // Earth radius in km
+	protected function set_ages(&$where)
+	{
+		$age_from = $this->data['age_from'];
+		$age_to = $this->data['age_to'];
 
-        $dLat = (($q[0] - $p[0]) * pi() / 180);
-        $dLon = (($q[1] - $p[1]) * pi() / 180);
-        $a = sin($dLat / 2) * sin($dLat / 2) +
-                cos($p[0] * pi() / 180) * cos($q[0] * pi() / 180) *
-                sin($dLon / 2) * sin($dLon / 2);
-        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+		if (!empty($age_from) && !empty($age_to)) {
+			$where .= "AND (`age` BETWEEN $age_from AND $age_to) ";
+		}
+		else if (!empty($age_from)) {
+			$where .= "AND `age` >= $age_from ";
+		}
+		else if (!empty($age_to)) {
+			$where .= "AND `age` <= $age_to ";
+		}
+	}
 
-        return $R * $c;
-    }
+	protected function set_interests(&$sql)
+	{
+		$db = new DB();
+		//фильтр показывать профили с интересами
+		// [{"id":1,"value":1},{"id":2,"value":0},{"id":3,"value":1}]
+		$interests_on = json_decode($this->data['interests_on']);
+		if (!empty($interests_on))
+		{
+			// Выбираем id пользователей с соответств. интересами
+			$sql .= 'AND ' . '(';
+			$c = 0;
+			$count_on = count($interests_on);
+			foreach ($interests_on as $row) {
+				$sql .= "(ui.interest_id = {$row->id} AND ui.value = {$row->value}) ";
+				if($count_on != $c + 1){
+					$sql .= 'OR ';
+				}
+				$c++;
+			}
+			$sql .= ') ';
+		}
+
+		//фильтр не показывать профили с интересами
+		// [{"id":1,"value":1},{"id":2,"value":0},{"id":3,"value":1}]
+		$interests_off = json_decode($this->data['interests_off']);
+		if (!empty($interests_off))
+		{
+			// Выбираем id пользователей с соответств. интересами
+			$sql .= 'AND ' . '(';
+			$c = 0;
+			$count_off = count($interests_off);
+			foreach ($interests_off as $row) {
+				$sql .= "(ui.interest_id IS NULL OR (ui.interest_id = {$row->id} AND ui.value <> {$row->value})) ";
+				if($count_off != $c + 1){
+					$sql .= 'OR ';
+				}
+				$c++;
+			}
+			$sql .= ') ';
+		}
+	}
+	// 2й вариант не используемый
+	protected function set_interests2(&$sql)
+	{
+		$db = new DB();
+		//фильтр показывать профили с интересами
+		// [{"id":1,"value":1},{"id":2,"value":0},{"id":3,"value":1}]
+		$interests_on = json_decode($this->data['interests_on']);
+		if (!empty($interests_on))
+		{
+			// Выбираем id пользователей с соответств. интересами
+			$inter_sql_on = 'SELECT `user_id` FROM `user_interests` WHERE ';
+			$c = 0;
+			foreach ($interests_on as $row) {
+				$inter_sql_on .= "(`interest_id` = {$row->id} AND `value` = {$row->value}) ";
+				if(count($interests_on) != $c + 1){
+					$inter_sql_on .= 'OR ';
+				}
+				$c++;
+			}
+
+			$interests_user_ids = $db::getColumn($inter_sql_on);
+			$interests_user_ids_str = implode(',', $interests_user_ids);
+   			if(!empty($interests_user_ids_str)){
+				$sql .= "AND `id` IN ($interests_user_ids_str) ";
+			}
+		}
+
+		//фильтр не показывать профили с интересами
+		// [{"id":1,"value":1},{"id":2,"value":0},{"id":3,"value":1}]
+		$interests_off = json_decode($this->data['interests_off']);
+		if (!empty($interests_off))
+		{
+			// Выбираем id пользователей с соответств. интересами
+			$inter_sql_off = 'SELECT `user_id` FROM `user_interests` WHERE 1 ';
+			foreach ($interests_off as $row) {
+				$inter_sql_off .= "AND (`id` = {$row->id} AND `value` = {$row->value}) ";
+			}
+			$interests_user_ids = $db::getColumn($inter_sql_off);
+			$interests_user_ids_str = implode(',', $interests_user_ids);
+
+			if(!empty($interests_user_ids_str)){
+				$sql .= "AND `id` NOT IN ($interests_user_ids_str) ";
+			}
+		}
+	}
 }
